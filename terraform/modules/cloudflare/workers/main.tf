ad40dd71c6e91daa290489b20879c98cce4d59a4
SEB Administrator <admin@localhost>
2026-02-21 18:58:35 +0300

    g

--- terraform/modules/cloudflare/workers/main.tf
+++ terraform/modules/cloudflare/workers/main.tf
@@ -25,14 +25,3 @@ resource "cloudflare_workers_script" "ghost_proxy" {
     text = "https://${var.c2_backend_url}"
   }
 }
-# Enable the worker on the *.workers.dev subdomain
-resource "cloudflare_worker_domain" "workers_dev" {
-  account_id = var.cloudflare_account_id
-  hostname   = "${cloudflare_worker_script.ghost_proxy.name}.YOUR_WORKERS_SUBDOMAIN.workers.dev"
-  service    = cloudflare_worker_script.ghost_proxy.name
-  zone_id    = "" # Not needed for workers.dev
-  
-  # Note: Currently, Terraform requires a custom domain to use cloudflare_worker_domain easily.
-  # If you don't have a domain, Cloudflare automatically enables the worker on 
-  # <script-name>.<your-subdomain>.workers.dev by default when the script is uploaded!
-}
